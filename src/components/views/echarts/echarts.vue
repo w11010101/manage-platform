db4faa1ea339ecfdb11fe8d9a3af90f447069ec9
大馒头小咸菜 <w11010101@hotmail.com>
2018-12-03 10:04:11 +0800

    add echarts

--- src/components/views/echarts/echarts.vue
+++ src/components/views/echarts/echarts.vue
@@ -1,49 +1,43 @@
 <template>
     <div class="child">
-        <Alert show-icon style='font-size:16px;'>依赖 iview ，若想查看相关方法、属性和事件，请点击 Docs 跳转查看；
+        <!-- <Alert show-icon style='font-size:16px;'>依赖 iview ，若想查看相关方法、属性和事件，请点击 Docs 跳转查看；
           <template slot="desc">注意：非 template/render 模式下，把<code>&lt;{{name.replace(/^\S/,s=>s.toUpperCase())}}&gt;</code>改成 <code>&lt;i-{{name}}&gt;</code>。</template>
+        </Alert> -->
+        <Alert show-icon style='font-size:16px;'>如果引入echarts.js是静态文件，勿忘配置webpack.base.conf.js;
+            <template slot="desc">注意：echarts.js 为本地静态文件，第三方插件，如果该插件是按照模块化开发（commonjs、amd、cmd、es6）；可以通过相应的模块调用方法，来使用；
+            </template>
         </Alert>
         <!-- example 1 -->
         <Divider orientation="left">基础用法</Divider>
-        <Tooltip content="Here is the prompt text（这里是提示信息）">
-            如果引入echarts.js是静态文件，勿忘配置webpack.base.conf.js; 
-        </Tooltip>
+        <div id="echarts"></div>
         <!-- code -->
         <div class="demo-express">
             <h3 class="demo-express-title">HTML<Icon type="ios-copy" size='20' class="copyBtn"  data-clipboard-target="#part1-html"/></h3>
-            <pre class="codeBox" id='part1-html'>{{htmlCode[0]}}</pre>
+            <pre class="codeBox" id='part1-html'>{{htmlCode}}</pre>
         </div>
-        <!-- example 2 -->
-        <Divider orientation="left">基础用法</Divider>
-        
         <!-- code -->
         <div class="demo-express">
-            <h3 class="demo-express-title">HTML<Icon type="ios-copy" size='20' class="copyBtn"  data-clipboard-target="#part2-html"/></h3>
-            <pre class="codeBox" id='part2-html'>{{}}</pre>
+            <h3 class="demo-express-title">JS<Icon type="ios-copy" size='20' class="copyBtn"  data-clipboard-target="#part1-js"/></h3>
+            <pre class="codeBox" id='part1-js'>{{jsCode}}</pre>
         </div>
-        <div class="demo-express">
-            <h3 class="demo-express-title">CSS<Icon type="ios-copy" size='20' class="copyBtn"  data-clipboard-target="#part2-css"/></h3>
-            <pre class="codeBox" id='part2-css'>{{}}</pre>
-        </div>
-        
 
     </div>
 </template>
 
 <script>
-import {tooltip} from '@/js/tool/part.js';
-let {htmlCode,cssCode} = {htmlCode:tooltip.html,cssCode:tooltip.css};
+import {myEcharts} from '@/js/tool/part.js';
+let {htmlCode,jsCode} = {htmlCode:myEcharts.html,jsCode:myEcharts.js};
 
 function toStringify(obj){
     return typeof obj === 'object'?JSON.stringify(obj):obj;
 }
 export default {
-    name: 'example-tooltip',
+    name: 'example-echarts',
     data () {
         return {
-            value: 'this is tooltip.vue 。',
+            value: 'this is echarts.vue 。',
             htmlCode,
-            cssCode,
+            jsCode,
             
         }
     },
@@ -58,7 +52,29 @@ export default {
     },
     mounted(){
         this.$nextTick(function(){
-            console.log(this.$route);
+            console.log(echarts);
+            var myChart = echarts.init(document.getElementById('echarts'));
+            var option = {
+                title: {
+                    text: 'ECharts 入门示例'
+                },
+                tooltip: {},
+                legend: {
+                    data:['销量']
+                },
+                xAxis: {
+                    data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+                },
+                yAxis: {},
+                series: [{
+                    name: '销量',
+                    type: 'bar',
+                    data: [5, 20, 36, 10, 10, 20]
+                }]
+            };
+
+            // 使用刚指定的配置项和数据显示图表。
+            myChart.setOption(option);
         })
     },
     methods:{
@@ -69,18 +85,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    .top,.bottom{
-        text-align: center;
-    }
-    .center{
-        width: 300px;
-        margin: 10px auto;
-        overflow: hidden;
-    }
-    .center-left{
-        float: left;
-    }
-    .center-right{
-        float: right;
+    #echarts{
+        width: 500px;
+        height: 500px;
+        padding: 10px;
+        border:1px solid #bbb;
+        border-radius:5px;
     }
 </style>
--- src/js/tool/part.js
+++ src/js/tool/part.js
@@ -1446,5 +1446,30 @@ export const badge = {
     ]
 }
 
+export const myEcharts = {
+    html:`<div id="echarts"></div>`,
+    css:``,
+    js:`var myChart = echarts.init(document.getElementById('echarts'));
+    var option = {
+        title: {
+            text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        legend: {
+            data:['销量']
+        },
+        xAxis: {
+            data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+        },
+        yAxis: {},
+        series: [{
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+        }]
+    };
 
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);`
+}
 
